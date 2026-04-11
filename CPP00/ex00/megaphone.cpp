d268b31103f28dea88c9fc4c509d2e2b4bd6ad2a
Miraiy <66632625+vde-maga@users.noreply.github.com>
2026-04-11 23:08:08 +0100

    cpp00/ex00 done (simple hello world + makefile update)

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <ctype.h>
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			std::cout << (char)toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
