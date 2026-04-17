c66e8582173b617b3731088500baef48410e4891
vde-maga <Tiago815842@gmail.com>
2026-04-17 18:54:59 +0100

    WIP ex03

---
+++ CPP01/ex03/includes/HumanB.hpp
@@ -0,0 +1,17 @@
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+# include "Weapon.h"
+
+class	HumanB
+{
+	private:
+		std::string	_name;
+		Weapon	*_weapon;
+	public:
+		HumanB(std::string name);
+		void	setWeapon(Weapon &weapon);
+		void	attack(void);
+};
+
+#endif
---
+++ CPP01/ex03/srcs/HumanA.cpp
@@ -0,0 +1,11 @@
+#include "HumanA.hpp"
+#include <iostream>
+
+HumanA::HumanA(std::string name, Weapon &weapon) : _name(name), _weapon(weapon)
+{
+}
+
+void	HumanA::attack(void)
+{
+	std::cout << this->_name << " attacks with their " << this->weapon.getType() << std::endl;
+}
